940ef0a4c9b832c97afc3271f99f1af80cfe43fd
Marisa <marisavandellos@gmail.com>
2023-09-10 11:18:50 -0700

    fixed a bug

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,6 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        
           { 
             puppies.map((puppy) => {
               return <p>{puppy.name}</p>
